6228b47b7e7c70f3348b1b16a45decae81613b20
kolliasl21 <163063295+kolliasl21@users.noreply.github.com>
2025-02-13 00:15:02 +0200

    Upload .service files, update README

--- README.md
+++ README.md
@@ -5,5 +5,14 @@
 - volume_controller.py : Control system volume automatically.
 - file_remover.py : Remove/Keep files in current working directory that match input directory or text file. Use with caution!
 While transcoding a large amount of files with convert2mp3.py use the --enable-log flag to log what files were successfully transcoded.
-In case of a crash/disconnect use "file_remover.py convert2mp3.log -k" to remove corrupted files and run convert2mp3.py again to resume
+In case of a crash/disconnect use "file_remover.py /home/user/scripts/convert2mp3.log -k" to remove corrupted files and run convert2mp3.py again to resume
 transcoding.
+- musicplayer.sh : Modify this file to change input arguments and running parameters.
+
+## Enable services
+
+- Create .timer and .service files in /etc/systemd/user/
+- Reload systemd daemons: $ systemctl --user daemon-reload
+- Enable timer: $ systemctl --user enable musicplayerd.timer --now
+- Enable services: $ systemctl --user enable volume-controller.service musicplayerd.service --now
+- Start services if auto-login is disabled: loginctl enable-linger user
---
+++ musicplayer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DAY=$(date | awk '{print $1}')
+
+musicplayer() {
+        /home/user/scripts/musicplayer.py /home/user/"$1" -r -vv -p ffplay -t "$2" "$3" --no-controls --fade 5 --enable-log --terminate $4
+}
+
+if   [[ $DAY == 'Tue' || $DATE == 'Thu' ]]; then
+        musicplayer "Music/playlist1" "05:10" "09:30" "--wait"
+        musicplayer "Music/playlist2" "05:10" "12:00" "--wait"
+        musicplayer "Music/playlist1" "05:10" "22:00"
+elif [[ $DAY == 'Wed' || $DATE == 'Fri' ]]; then
+        musicplayer "Music/pop2024" "05:10" "11:00" "--wait"
+        musicplayer "Music/playlist1" "05:10" "13:00" "--wait"
+        musicplayer "Music/pop2024" "05:10" "19:00" "--wait"
+        musicplayer "Music/playlist2" "05:10" "22:00"
+elif [[ $DAY == 'Sat' ]]; then
+        musicplayer "Music/playlist1" "05:10" "22:00"
+elif [[ $DAY == 'Sun' ]]; then
+        musicplayer "Music/2010s_hits" "05:10" "11:30" "--wait"
+        musicplayer "Music/playlist1" "05:10" "22:00"
+else
+        musicplayer "Music/playlist1" "05:10" "10:00" "--wait"
+        musicplayer "Music/2010s_hits" "05:10" "13:00" "--wait"
+        musicplayer "Music/playlist1" "05:10" "22:00"
+fi
