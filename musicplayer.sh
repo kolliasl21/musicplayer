359d0bb4c4a07501422aeebab7d9b455ba05906a
kolliasl21 <163063295+kolliasl21@users.noreply.github.com>
2025-02-20 23:49:10 +0200

    Bug fix

--- musicplayer.sh
+++ musicplayer.sh
@@ -6,11 +6,11 @@ musicplayer() {
         /home/user/scripts/musicplayer.py /home/user/"$1" -r -vv -p ffplay -t "$2" "$3" --no-controls --fade 5 --enable-log --terminate $4
 }
 
-if   [[ $DAY == 'Tue' || $DATE == 'Thu' ]]; then
+if   [[ $DAY == 'Tue' || $DAY == 'Thu' ]]; then
         musicplayer "Music/playlist1" "05:10" "09:30" "--wait"
         musicplayer "Music/playlist2" "05:10" "12:00" "--wait"
         musicplayer "Music/playlist1" "05:10" "22:00"
-elif [[ $DAY == 'Wed' || $DATE == 'Fri' ]]; then
+elif [[ $DAY == 'Wed' || $DAY == 'Fri' ]]; then
         musicplayer "Music/pop2024" "05:10" "11:00" "--wait"
         musicplayer "Music/playlist1" "05:10" "13:00" "--wait"
         musicplayer "Music/pop2024" "05:10" "19:00" "--wait"
